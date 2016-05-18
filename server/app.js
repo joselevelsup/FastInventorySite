491fc2949ed1048627bfd579bd4e3007fccc6e00
joselevelsup <hipnotickid12@gmail.com>
2016-05-18 11:20:10 -0400

    First Commit to Heroku

---
+++ server/app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var Firebase = require("firebase");
+
+app.use(express.static(__dirname + '/client')); //Serves the necessary files for the frontend
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+
+app.get("*", function(request, respond){
+  respond.sendFile(__dirname+'/client/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
+console.log("Server Running");
